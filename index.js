4087d6698bbeb178a41d9fa36a1bd24d64c0d0ac
Saravanan S <saravanans.design@gmail.com>
2023-05-15 12:36:45 +0530

    mqtt connection

--- index.js
+++ index.js
@@ -1,7 +1,64 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.greet = void 0;
-function greet(name) {
-    console.log("Hello, ".concat(name, "!"));
-}
-exports.greet = greet;
+exports.MqttConnection = void 0;
+var mqtt_1 = require("mqtt");
+var MqttConnection = /** @class */ (function () {
+    function MqttConnection(brokerUrl, options) {
+        this.brokerUrl = brokerUrl;
+        this.options = options;
+        this.client = (0, mqtt_1.connect)(brokerUrl, options);
+        this.registerEventHandlers();
+    }
+    MqttConnection.prototype.registerEventHandlers = function () {
+        this.client.on('connect', this.onConnect);
+        this.client.on('message', this.onMessage);
+        this.client.on('error', this.onError);
+        this.client.on('close', this.onClose);
+        this.client.on('disconnect', this.onDisconnect);
+    };
+    MqttConnection.prototype.onConnect = function () {
+        console.log('Connected to MQTT broker');
+    };
+    MqttConnection.prototype.onMessage = function (topic, message) {
+        console.log('Received message from topic:', topic);
+        console.log('Message:', message.toString());
+    };
+    MqttConnection.prototype.onError = function (error) {
+        console.error('MQTT error:', error);
+    };
+    MqttConnection.prototype.onClose = function () {
+        console.log('MQTT connection closed');
+    };
+    MqttConnection.prototype.onDisconnect = function () {
+        console.log('MQTT client disconnected');
+    };
+    MqttConnection.prototype.subscribe = function (topic, callback) {
+        this.client.subscribe(topic, { qos: 0 }, function (err, granted) {
+            if (err) {
+                console.error('Error while subscribing:', err);
+            }
+            else {
+                console.log('Subscribed to topic:', topic);
+            }
+        });
+        this.client.on('message', callback);
+    };
+    MqttConnection.prototype.publish = function (topic, message) {
+        this.client.publish(topic, message);
+    };
+    MqttConnection.prototype.unsubscribe = function (topic) {
+        this.client.unsubscribe(topic, function (err) {
+            if (err) {
+                console.error('Error while unsubscribing:', err);
+            }
+            else {
+                console.log('Unsubscribed from topic:', topic);
+            }
+        });
+    };
+    MqttConnection.prototype.end = function () {
+        this.client.end();
+    };
+    return MqttConnection;
+}());
+exports.MqttConnection = MqttConnection;
--- index.ts
+++ index.ts
@@ -1,4 +1,69 @@
-export function greet(name: string): void {
-    console.log(`Hello, ${name}!`);
+import { connect,MqttClient, IClientOptions } from 'mqtt';
+
+export class MqttConnection {
+  private client: MqttClient;
+
+  constructor(private brokerUrl: string, private options?: IClientOptions) {
+    this.client = connect(brokerUrl, options);
+    this.registerEventHandlers();
   }
-  
+
+  private registerEventHandlers(): void {
+    this.client.on('connect', this.onConnect);
+    this.client.on('message', this.onMessage);
+    this.client.on('error', this.onError);
+    this.client.on('close', this.onClose);
+    this.client.on('disconnect', this.onDisconnect);
+  }
+
+  private onConnect(): void {
+    console.log('Connected to MQTT broker');
+  }
+
+  private onMessage(topic: string, message: Buffer): void {
+    console.log('Received message from topic:', topic);
+    console.log('Message:', message.toString());
+  }
+
+  private onError(error: Error): void {
+    console.error('MQTT error:', error);
+  }
+
+  private onClose(): void {
+    console.log('MQTT connection closed');
+  }
+
+  private onDisconnect(): void {
+    console.log('MQTT client disconnected');
+  }
+
+  public subscribe(topic: string, callback: (topic: string, message: Buffer) => void): void {
+    this.client.subscribe(topic, { qos: 0 }, (err, granted) => {
+      if (err) {
+        console.error('Error while subscribing:', err);
+      } else {
+        console.log('Subscribed to topic:', topic);
+      }
+    });
+
+    this.client.on('message', callback);
+  }
+
+  public publish(topic: string, message: string): void {
+    this.client.publish(topic, message);
+  }
+
+  public unsubscribe(topic: string): void {
+    this.client.unsubscribe(topic, (err) => {
+      if (err) {
+        console.error('Error while unsubscribing:', err);
+      } else {
+        console.log('Unsubscribed from topic:', topic);
+      }
+    });
+  }
+
+  public end(): void {
+    this.client.end();
+  }
+}
